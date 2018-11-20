19650dd0bae8684129290899a5440470af95c4b1
Hcuixia <15617829662@163.com>
2018-11-20 20:46:25 +0800

    first add

---
+++ js/base.js
@@ -0,0 +1,10 @@
+/**
+ * Created by web on 2018/8/25.
+ */
+/*ÒıÈëÍ·Î²js*/
+$(document).ready(function () {
+    $('.my_header').load('header.html');
+    $('.my_footer').load('footer.html');
+});
+
+
